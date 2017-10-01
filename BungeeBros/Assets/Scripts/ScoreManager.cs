1a1dea59da32ebee5c75231d10f860def991fb79
MAIN\ngb7532 <nategribble@gmail.com>
2017-10-01 12:13:19 -0400

    Refactor misspelling of a word.

--- BungeeBros/Assets/Scripts/ScoreManager.cs
+++ BungeeBros/Assets/Scripts/ScoreManager.cs
@@ -35,14 +35,14 @@ public class ScoreManager : MonoBehaviour
             // Get the worst score from all players in the team
             foreach (PlayerController player in team.players)
             {
-                if (player.GetPlayerSocre() >= MAX_LENGTH)
+                if (player.GetPlayerScore() >= MAX_LENGTH)
                 {
                     currentWorst = -1;
                     break;
                 }
 
-                if (player.GetPlayerSocre() < currentWorst)
-                    currentWorst = player.GetPlayerSocre();
+                if (player.GetPlayerScore() < currentWorst)
+                    currentWorst = player.GetPlayerScore();
             }
 
             if (currentWorst > best)
@@ -72,7 +72,7 @@ public class ScoreManager : MonoBehaviour
             // Get the sum of scores of all players in the team
             foreach (PlayerController player in team.players)
             {
-                currentSum += player.GetPlayerSocre();
+                currentSum += player.GetPlayerScore();
                 if (currentSum >= MAX_LENGTH)
                 {
                     currentSum = -1;
@@ -107,7 +107,7 @@ public class ScoreManager : MonoBehaviour
             // Get the worst score from all players in the team
             foreach (PlayerController player in team.players)
             {
-                currentSum += player.GetPlayerSocre();
+                currentSum += player.GetPlayerScore();
                 if (currentSum >= MAX_LENGTH)
                 {
                     currentSum = -1;
