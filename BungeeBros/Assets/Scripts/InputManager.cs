cff95bca2fadb353646ee89591598e86ed12cd0a
MAIN\ngb7532 <nategribble@gmail.com>
2017-10-01 11:17:21 -0400

    Add more player behaviors.

--- BungeeBros/Assets/Scripts/InputManager.cs
+++ BungeeBros/Assets/Scripts/InputManager.cs
@@ -6,8 +6,6 @@ public class InputManager : MonoBehaviour {
 
     public static InputManager instance;
 
-    public bool devMode;
-
 	// Use this for initialization
 	void Start () {
 		if (instance == null)
@@ -20,27 +18,13 @@ public class InputManager : MonoBehaviour {
         }
 	}
 
-    public float GetHorizontalForPlayer (int playerNumber)
-    {
-        if (devMode)
-            playerNumber = 1;
-
-        return Input.GetAxis("P" + playerNumber +"_Horizontal");
-    }
-
     public bool GetButtonDownForPlayer (int playerNumber, string button)
     {
-        if (devMode)
-            playerNumber = 1;
-
-        return Input.GetButtonDown("P" + playerNumber + "_" + button);
+        return Input.GetButtonDown(button + "_" + "P" + playerNumber);
     }
 
     public bool GetButtonUpForPlayer(int playerNumber, string button)
     {
-        if (devMode)
-            playerNumber = 1;
-
-        return Input.GetButtonUp("P" + playerNumber + "_" + button);
+        return Input.GetButtonDown(button + "_" + "P" + playerNumber);
     }
 }
