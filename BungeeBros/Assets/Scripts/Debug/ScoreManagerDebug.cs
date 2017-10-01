40637bf0d676ddcea5ee57a66b36decb74e05c1b
CapstoneLab\Akshay Rachapudi <akshay.rachapudi@gmail.com>
2017-10-01 01:32:55 -0400

    - Added Score Manager that gets winner - Added Team Manager that divides players into teams

---
+++ BungeeBros/Assets/Scripts/Debug/ScoreManagerDebug.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ScoreManager))]
+public class ScoreManagerDebug : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ScoreManager scoreMan = target as ScoreManager;
+        
+        if (GUILayout.Button("UpdateScores"))
+        {
+            scoreMan.UpdateScores();
+        }
+    }
+}
---
+++ BungeeBros/Assets/Scripts/TeamManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameUtilities;
+
+public class TeamManager : MonoBehaviour {
+
+    public List<int> playerToTeam = new List<int>();
+    public List<Player> players = new List<Player>();
+    public List<Team> teams = new List<Team>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        int playerNumber = 1;
+        foreach (int teamNumber in playerToTeam)
+        {
+            players.Add(new Player(playerNumber, teamNumber));
+            playerNumber++;
+        }
+
+        MakeTeams();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MakeTeams()
+    {
+        foreach(Player player in players)
+        {
+            if (teams.Count < player.teamNumber)
+            {
+                for (int i = teams.Count; i < player.teamNumber; i++)
+                    teams.Add(new Team(i + 1));
+            }
+
+            teams[player.teamNumber - 1].players.Add(player);
+        }
+    }
+}
