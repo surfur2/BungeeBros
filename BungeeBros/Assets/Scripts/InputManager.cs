8e18d5b16841b34bdf7380133687a12b572b1d69
MAIN\ngb7532 <nategribble@gmail.com>
2017-10-01 10:40:56 -0400

    Input Manager Base

---
+++ BungeeBros/Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public static InputManager instance;
+
+    public bool devMode;
+
+	// Use this for initialization
+	void Start () {
+		if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    public float GetHorizontalForPlayer (int playerNumber)
+    {
+        if (devMode)
+            playerNumber = 1;
+
+        return Input.GetAxis("P" + playerNumber +"_Horizontal");
+    }
+
+    public bool GetButtonDownForPlayer (int playerNumber, string button)
+    {
+        if (devMode)
+            playerNumber = 1;
+
+        return Input.GetButtonDown("P" + playerNumber + "_" + button);
+    }
+
+    public bool GetButtonUpForPlayer(int playerNumber, string button)
+    {
+        if (devMode)
+            playerNumber = 1;
+
+        return Input.GetButtonUp("P" + playerNumber + "_" + button);
+    }
+}
